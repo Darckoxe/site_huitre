d3a2a4adb7caa8088415acb19cc42f1a84e0111c
Darckoxe <alexandredu_44@hotmail.fr>
2018-02-14 22:34:49 +0100

    MàJ

--- accueil.php
+++ accueil.php
@@ -2,12 +2,34 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="web/css/reset.css">
     <link rel="stylesheet" href="web/css/style.css">
     <script src="web/js/jquery.js"></script>
     <title>Accueil - Fleur des flots</title>
 </head>
 <body>
     <?php include "include/menu.html" ?>
+    <p>Bienvenue au bar à huîtres Fleurs des Flots au Port des Brochets à Bouin.<br/>
+        Découvrez nos assiettes d'huîtres, notre assiette de la mer, seiche, ou encore nos sardines de <br/>
+        Saint-Gilles Croix-de-Vie et NOUVEAUTE 2016 PLATEAUX DE FRUITS DE MER sur réservation.</p>
+    <div class="container2">
+        <figure>
+            <img src="web/img/chat.jpg"/>
+            <figcaption>Un chat mignon</figcaption>
+        </figure>
+        <figure>
+            <img src="web/img/image1.jpg"/>
+            <figcaption>Robert qui drague Martine</figcaption>
+        </figure>
+        <figure>
+            <img src="web/img/image2.jpg"/>
+            <figcaption>Une famille de bonne facture</figcaption>
+        </figure>
+        <figure>
+            <img src="web/img/image3.jpg"/>
+            <figcaption>Un placement de produit</figcaption>
+        </figure>
+    </div>
     <?php include "include/footer.html" ?>
     <script src="web/js/main.js" charset="utf-8"></script>
 </body>
--- historique.php
+++ historique.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="web/css/reset.css">
     <link rel="stylesheet" href="web/css/style.css">
     <script src="web/js/jquery.js" charset="utf-8"></script>
     <title>Historique - Fleur des Flots</title>
