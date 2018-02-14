9a97460b97fa970bf85e3495038796b437159f57
Darckoxe <alexandredu_44@hotmail.fr>
2018-02-14 14:53:41 +0100

    .

--- accueil.php
+++ accueil.php
@@ -2,11 +2,13 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Accueil - Fleur des flots</title>
+    <link rel="stylesheet" href="web/css/style.css">
     <script src="web/js/jquery.js"></script>
+    <title>Accueil - Fleur des flots</title>
 </head>
 <body>
     <?php include "include/menu.html" ?>
     <?php include "include/footer.html" ?>
+    <script src="web/js/main.js" charset="utf-8"></script>
 </body>
 </html>
